c2d7f2fca9562e126d5cb51ed2b8e5425ab148ec
root <root@zhanglu.(none)>
2016-08-23 20:02:34 +0800

    first

---
+++ a.cpp
@@ -0,0 +1,32 @@
+#pragma comment(linker,"/STACK:102400000,102400000")
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <list>
+#include <deque>
+
+//#define LOCAL
+
+#define pr(x) cout << x << "  "
+#define prln(x) cout << x << endl
+#define	INF 1e9
+#define eps 1e-12
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL
+	freopen("input","r",stdin);
+#endif
+
+
+	while(scanf("",))
+}
+
